438b471fa8aa795797c5a011bfa4794ae04eb0b6
TuDang1 <dangquoctuhn@gmail.com>
2022-12-12 18:53:56 +0700

    Update seeder

--- database/UserSeeder.js
+++ database/UserSeeder.js
@@ -9,41 +9,41 @@ exports.UserSeeder = async function () {
   try {
     const data = [
       {
-        _id: mongoose.Types.ObjectId('63536c4ac283db80849f60d5'),
-        email: 'admin@gmail.com',
-        name: 'Anh Nguyễn Thế',
-        phone: '0987654321',
-        role: config.get('role.admin'),
+        _id: mongoose.Types.ObjectId("63536c4ac283db80849f60d5"),
+        email: "admin@gmail.com", // 12345678
+        name: "Anh Nguyễn Thế",
+        phone: "0987654321",
+        role: config.get("role.admin"),
       },
       {
-        _id: mongoose.Types.ObjectId('6353996526705194525e1043'),
-        email: 'editor@gmail.com',
-        name: 'Nguyễn Thế Trung',
-        phone: '0987654321',
-        role: config.get('role.editor'),
+        _id: mongoose.Types.ObjectId("6353996526705194525e1043"),
+        email: "editor@gmail.com", // 123456789
+        name: "Nguyễn Thế Trung",
+        phone: "0987654321",
+        role: config.get("role.editor"),
       },
       {
-        _id: mongoose.Types.ObjectId('6353993e26705194525e103f'),
-        email: 'sale@gmail.com',
-        name: 'Nguyễn Thế Trung',
-        phone: '0987654321',
-        role: config.get('role.sale'),
+        _id: mongoose.Types.ObjectId("6353993e26705194525e103f"),
+        email: "sale@gmail.com", // 123456789
+        name: "Nguyễn Thế Trung",
+        phone: "0987654321",
+        role: config.get("role.sale"),
       },
       {
-        _id: mongoose.Types.ObjectId('6357ef9627f89e3238d20185'),
-        email: 'trungnguyenthe@gmail.com',
-        name: 'Nguyễn Thế Trung',
-        phone: '0987654321',
-        role: config.get('role.sale'),
+        _id: mongoose.Types.ObjectId("6357ef9627f89e3238d20185"),
+        email: "trungnguyenthe@gmail.com",
+        name: "Nguyễn Thế Trung",
+        phone: "0987654321",
+        role: config.get("role.sale"),
       },
       {
-        _id: mongoose.Types.ObjectId('6357efa027f89e3238d20189'),
-        email: 'trungnguyen1998@gmail.com',
-        name: 'Nguyễn Thế Trung',
-        phone: '0987654321',
+        _id: mongoose.Types.ObjectId("6357efa027f89e3238d20189"),
+        email: "trungnguyen1998@gmail.com",
+        name: "Nguyễn Thế Trung",
+        phone: "0987654321",
         password:
-          '$2b$10$/GVZNx4xbFW/lnTRCa.Wo.dbyQdOojJG9yBNOKxeGet6HV7Z4sRka',
-        role: config.get('role.sale'),
+          "$2b$10$/GVZNx4xbFW/lnTRCa.Wo.dbyQdOojJG9yBNOKxeGet6HV7Z4sRka",
+        role: config.get("role.sale"),
       },
     ];
 
